69dabe4b3e6c0e7ebfbc84641b4ac044c4989c6b
JP <phillemieux66@hotmail.com>
2020-06-04 14:19:29 -0400

    Refactor tests for content

--- __tests__/integration/content.test.ts
+++ __tests__/integration/content.test.ts
@@ -6,14 +6,18 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 
+import { disconnectDatabase, initDatabase } from '../../db/db';
+import { Collection } from '../../db/models/collection';
+import { CollectionUser } from '../../db/models/collection-user';
 import { Content } from '../../db/models/content';
-import { initDatabase, disconnectDatabase } from '../../db/db';
 import { getToken } from '../_auth/auth';
 
 dotenv.config({ path: 'config/.env.test' });
 jest.setTimeout(30000);
 
 let user1Token: string;
+let collection1Id: string;
+let collection2Id: string;
 
 beforeAll(async () => {
   await initDatabase();
@@ -21,19 +25,35 @@ beforeAll(async () => {
     process.env.AUTH0_USER_1_EMAIL,
     process.env.AUTH0_USER_1_PASSWORD,
   );
+
+  const collection = new Collection({
+    name: 'Test',
+  });
+  await collection.save();
+  collection1Id = collection._id.toString();
+
+  const collection2 = new Collection({
+    name: 'Test2',
+  });
+  await collection2.save();
+  collection2Id = collection2._id.toString();
+
+  const collectionUser = new CollectionUser({
+    collectionId: collection1Id,
+    userEmail: process.env.AUTH0_USER_1_EMAIL,
+  });
+  await collectionUser.save();
 });
 
 afterAll(async () => {
+  await Collection.deleteMany({});
+  await CollectionUser.deleteMany({});
   disconnectDatabase();
 });
 
 afterEach(async () => {
   await Content.deleteMany({});
 });
-
-/**
- * Content
- */
 
 const getTestContent = () => ({
   name: 'Test',
@@ -63,15 +83,19 @@ describe('POST /content', () => {
     const data = getTestContent();
 
     // Act
-    const res = await axios.post(process.env.API_URL + '/content', data, {
-      headers: { Authorization: `Bearer ${user1Token}` },
-    });
+    const res = await axios.post(
+      `${process.env.API_URL}/collections/${collection1Id}/content`,
+      data,
+      {
+        headers: { Authorization: `Bearer ${user1Token}` },
+      },
+    );
     const item = res.data;
 
     // Assert
     await expect(res.status).toBe(200);
     await expect(item.name).toEqual(data.name);
-    await expect(item.userEmail).toEqual(process.env.AUTH0_USER_1_EMAIL);
+    await expect(item.collectionId).toEqual(collection1Id);
   });
 
   test('Should fail if there is no token', async () => {
@@ -80,7 +104,10 @@ describe('POST /content', () => {
 
     // Act
     try {
-      const res = await axios.post(process.env.API_URL + '/content', data);
+      const res = await axios.post(
+        `${process.env.API_URL}/collections/${collection1Id}/content`,
+        data,
+      );
 
       // Assert
       await expect(res.status).not.toBe(200);
@@ -99,14 +126,18 @@ describe('UPDATE /content/:id', () => {
     const data = getTestContent();
     const initialItem = new Content({
       ...data,
-      userEmail: process.env.AUTH0_USER_1_EMAIL,
+      collectionId: collection1Id,
     });
     await initialItem.save();
     const newName = 'UPDATED';
 
     // Act
     const res = await axios.patch(
-      process.env.API_URL + '/content/' + initialItem._id.toString(),
+      process.env.API_URL +
+        '/collections/' +
+        collection1Id +
+        '/content/' +
+        initialItem._id.toString(),
       { name: newName },
       {
         headers: {
@@ -122,12 +153,12 @@ describe('UPDATE /content/:id', () => {
     await expect(item.name).toEqual(newName);
   });
 
-  test('Should return 404 if content is associated with another user', async () => {
+  test('Should return 404 if content is associated with another collection', async () => {
     // Arrange
     const data = getTestContent();
     const initialItem = new Content({
       ...data,
-      userEmail: process.env.AUTH0_USER_2_EMAIL,
+      collectionId: collection2Id,
     });
     await initialItem.save();
     const newName = 'UPDATED';
@@ -135,7 +166,7 @@ describe('UPDATE /content/:id', () => {
     // Act
     try {
       await axios.patch(
-        process.env.API_URL + '/content/' + initialItem._id,
+        `${process.env.API_URL}/collections/${collection1Id}/content/${initialItem._id}`,
         {
           name: newName,
         },
@@ -156,21 +187,17 @@ describe('UPDATE /content/:id', () => {
     const data = getTestContent();
     const initialItem = new Content({
       ...data,
-      userEmail: process.env.AUTH0_USER_2_EMAIL,
+      collectionId: collection1Id,
     });
     await initialItem.save();
     const newName = 'UPDATED';
+
     // Act
     try {
       await axios.patch(
-        process.env.API_URL + '/content/' + initialItem._id,
+        `${process.env.API_URL}/collections/${collection1Id}/content/${initialItem._id}`,
         {
           name: newName,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${user1Token}`,
-          },
         },
       );
     } catch (e) {
@@ -188,13 +215,13 @@ describe('DELETE /content/:id', () => {
     const data = getTestContent();
     const initialItem = new Content({
       ...data,
-      userEmail: process.env.AUTH0_USER_1_EMAIL,
+      collectionId: collection1Id,
     });
     await initialItem.save();
 
     // Act
     const res = await axios.delete(
-      process.env.API_URL + '/content/' + initialItem._id,
+      `${process.env.API_URL}/collections/${collection1Id}/content/${initialItem._id}`,
       {
         headers: {
           Authorization: `Bearer ${user1Token}`,
@@ -209,22 +236,25 @@ describe('DELETE /content/:id', () => {
     await expect(nullItem).toBeNull();
   });
 
-  test('Should return 404 if content is associated with another user', async () => {
+  test('Should return 404 if content is associated with an unauthorized collection', async () => {
     // Arrange
     const data = getTestContent();
     const initialItem = new Content({
       ...data,
-      userEmail: process.env.AUTH0_USER_2_EMAIL,
+      collectionId: collection2Id,
     });
     await initialItem.save();
 
     // Act
     try {
-      await axios.delete(process.env.API_URL + '/content/' + initialItem._id, {
-        headers: {
-          Authorization: `Bearer ${user1Token}`,
+      await axios.delete(
+        `${process.env.API_URL}/collections/${collection2Id}/content/${initialItem._id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${user1Token}`,
+          },
         },
-      });
+      );
     } catch (e) {
       await expect(e.response.status).toEqual(404);
       return;
@@ -238,17 +268,15 @@ describe('DELETE /content/:id', () => {
     const data = getTestContent();
     const initialItem = new Content({
       ...data,
-      userEmail: process.env.AUTH0_USER_2_EMAIL,
+      collectionId: collection1Id,
     });
     await initialItem.save();
 
     // Act
     try {
-      await axios.delete(process.env.API_URL + '/content/' + initialItem._id, {
-        headers: {
-          Authorization: `Bearer ${user1Token}`,
-        },
-      });
+      await axios.delete(
+        `${process.env.API_URL}/collections/${collection1Id}/content/${initialItem._id}`,
+      );
     } catch (e) {
       await expect(e.response.status).not.toBe(200);
       return;
@@ -259,29 +287,32 @@ describe('DELETE /content/:id', () => {
 });
 
 describe('GET /content', () => {
-  test('Should list content only for user', async () => {
+  test('Should list content only for collection', async () => {
     // Arrange
     const data = getTestContent();
     const dataLength = 3;
     for (let i = 0; i < dataLength; i++) {
       const item = new Content({
         ...data,
-        userEmail: process.env.AUTH0_USER_1_EMAIL,
+        collectionId: collection1Id,
       });
       await item.save();
     }
     const unauthorizedItem = new Content({
       ...data,
-      userEmail: process.env.AUTH0_USER_2_EMAIL,
+      collectionId: collection2Id,
     });
     await unauthorizedItem.save();
 
     // Act
-    const res = await axios.get(process.env.API_URL + '/content', {
-      headers: {
-        Authorization: `Bearer ${user1Token}`,
+    const res = await axios.get(
+      `${process.env.API_URL}/collections/${collection1Id}/content`,
+      {
+        headers: {
+          Authorization: `Bearer ${user1Token}`,
+        },
       },
-    });
+    );
 
     // Assert
     await expect(res.status).toBe(200);
@@ -293,13 +324,15 @@ describe('GET /content', () => {
     const data = getTestContent();
     const initialItem = new Content({
       ...data,
-      userEmail: process.env.AUTH0_USER_2_EMAIL,
+      collectionId: collection1Id,
     });
     await initialItem.save();
 
     // Act
     try {
-      await axios.get(process.env.API_URL + '/content');
+      await axios.get(
+        `${process.env.API_URL}/collections/${collection1Id}/content`,
+      );
     } catch (e) {
       await expect(e.response.status).not.toBe(200);
       return;
@@ -315,38 +348,44 @@ describe('GET /content/:id', () => {
     const data = getTestContent();
     const item = new Content({
       ...data,
-      userEmail: process.env.AUTH0_USER_1_EMAIL,
+      collectionId: collection1Id,
     });
     await item.save();
 
     // Act
-    const res = await axios.get(process.env.API_URL + '/content/' + item._id, {
-      headers: {
-        Authorization: `Bearer ${user1Token}`,
+    const res = await axios.get(
+      `${process.env.API_URL}/collections/${collection1Id}/content/${item._id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${user1Token}`,
+        },
       },
-    });
+    );
 
     // Assert
     await expect(res.status).toBe(200);
     await expect(res.data._id).toEqual(item._id.toString());
   });
 
-  test('Should not get content from other user', async () => {
+  test('Should not get content from unauthorized collection', async () => {
     // Arrange
     const data = getTestContent();
     const item = new Content({
       ...data,
-      userEmail: process.env.AUTH0_USER_2_EMAIL,
+      collectionId: collection2Id,
     });
     await item.save();
 
     // Act
     try {
-      await axios.get(process.env.API_URL + '/content/' + item._id, {
-        headers: {
-          Authorization: `Bearer ${user1Token}`,
+      await axios.get(
+        `${process.env.API_URL}/collections/${collection2Id}/content/${item._id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${user1Token}`,
+          },
         },
-      });
+      );
     } catch (e) {
       await expect(e.response.status).toBe(404);
     }
@@ -357,12 +396,14 @@ describe('GET /content/:id', () => {
     const data = getTestContent();
     const initialItem = new Content({
       ...data,
-      userEmail: process.env.AUTH0_USER_2_EMAIL,
+      collectionId: collection1Id,
     });
     await initialItem.save();
     // Act
     try {
-      await axios.get(process.env.API_URL + '/content/' + initialItem._id);
+      await axios.get(
+        `${process.env.API_URL}/collections/${collection1Id}/content/${initialItem._id}`,
+      );
     } catch (e) {
       // Assert
       await expect(e.response.status).not.toBe(200);
