f367f610f5b01340633603264a982eb8f511dd6f
JP <phillemieux66@hotmail.com>
2020-06-04 13:50:26 -0400

    Bug Fix

--- utils/verify-collection.ts
+++ utils/verify-collection.ts
@@ -3,24 +3,9 @@ import createHttpError from 'http-errors';
 import { CollectionUser } from '../db/models/collection-user';
 
 // TODO: Refactor in Middleware
-// export const verifyCollection = async (collectionId, userEmail) => {
-//   const [collectionUser, collection] = await Promise.all([
-//     CollectionUser.findOne({
-//       collectionId: collectionId,
-//       userEmail,
-//     }),
-//     Collection.findOne({
-//       _id: collectionId,
-//     }),
-//   ]);
-
-//   if (!collectionUser || !collection) {
-//     throw createHttpError(404, `Collection not found`);
-//   }
-// };
 
 export const verifyCollection = async (collectionId, userEmail) => {
-  const collectionUser = CollectionUser.findOne({
+  const collectionUser = await CollectionUser.findOne({
     collectionId: collectionId,
     userEmail,
   });
